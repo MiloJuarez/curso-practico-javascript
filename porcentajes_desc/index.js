e2f082ffa38b71901f29382037747da37454c904
Milo Juarez <ermilo98go@gmail.com>
2021-12-29 14:12:08 -0600

    Calculo de descuento con cupones

--- porcentajes_desc/index.js
+++ porcentajes_desc/index.js
@@ -6,9 +6,9 @@ function getPrecioConDescuento(precio, descuento) {
 }
 
 /*================== Obtiene el descuento y lo muestra en una etiqueta HTML =================*/
-function mostrarPrecioFinal(precio, descuento) {
+function mostrarPrecioFinal(precio, descuento, idlbl) {
   let precioConDescuento = getPrecioConDescuento(precio, descuento);
-  let lblPrecioFinal = document.getElementById("precioFinal");
+  let lblPrecioFinal = document.getElementById(idlbl);
   lblPrecioFinal.innerText = "$" + precioConDescuento.toFixed(2);
 }
 
@@ -21,7 +21,11 @@ let inputDescuento = document.getElementById("descuento");
  */
 inputPrecio.oninput = function () {
   if (inputDescuento.value) {
-    mostrarPrecioFinal(Number(this.value), Number(inputDescuento.value));
+    mostrarPrecioFinal(
+      Number(this.value),
+      Number(inputDescuento.value),
+      "precioFinal"
+    );
   }
 };
 
@@ -30,6 +34,60 @@ inputPrecio.oninput = function () {
  */
 inputDescuento.oninput = function () {
   if (inputPrecio.value) {
-    mostrarPrecioFinal(Number(inputPrecio.value), Number(this.value));
+    mostrarPrecioFinal(
+      Number(inputPrecio.value),
+      Number(this.value),
+      "precioFinal"
+    );
+  }
+};
+
+const cupones = [
+  { name: "merry_christmas", disscount: 45 },
+  { name: "end_of_year", disscount: 60 },
+  { name: "new_year", disscount: 30 },
+  { name: "valentine_days", disscount: 35 },
+  { name: "vip_pass", disscount: 50 },
+];
+
+/*================== Input de cupon de descuento =================*/
+let inputCupon = document.getElementById("cupon");
+let inputPOriginal = document.getElementById("precioOriginal");
+let lblCuponInvalido = document.getElementById("cuponInvalido");
+let cuponDescuento = document.getElementById("cuponDescuento");
+
+let validCupon;
+
+inputPOriginal.oninput = function () {
+  if (validCupon) {
+    mostrarPrecioFinal(
+      Number(this.value),
+      Number(validCupon.disscount),
+      "precioConCD"
+    );
+  }
+};
+
+inputCupon.oninput = function () {
+  let cuponName = this.value;
+  validCupon = cupones.find((cupon) => cupon.name === cuponName);
+
+  if (!validCupon) {
+    let lblPrecioFinal = document.getElementById("precioConCD");
+    lblPrecioFinal.innerText = "$0.00";
+    lblCuponInvalido.style.display = "block";
+    cuponDescuento.innerText = "0";
+    validCupon = null;
+  } else {
+    lblCuponInvalido.style.display = "none";
+    cuponDescuento.innerText = validCupon.disscount;
+
+    if (inputPOriginal.value) {
+      mostrarPrecioFinal(
+        Number(inputPOriginal.value),
+        Number(validCupon.disscount),
+        "precioConCD"
+      );
+    }
   }
 };
