4ba49211ac1b44b6d45037e43b1b66857535e4c2
Milo Juarez <ermilo98go@gmail.com>
2022-01-03 17:49:34 -0600

    Calculo de la mediana

--- operaciones_aritm/index.js
+++ operaciones_aritm/index.js
@@ -1,3 +1,6 @@
+/**==============================================
+ *                CALCULO DEL PROMEDIO
+ *=============================================**/
 const lista = [100, 200, 300, 500];
 
 let sumaLista = 0;
@@ -21,4 +24,32 @@ function calcularMediaAritmetica(lista) {
     const promedioLista = sumaLista / lista.length;
 
     return promedioLista;
+}
+
+/**==============================================
+ *                CALCULO DE LA MEDIANA
+ *=============================================**/
+
+const lista2 = [100, 400, 10000, 200, 500, 600, 40000000];
+const mitadLista2 = parseInt(lista2.length / 2);
+
+function sortList(lista) {
+    return lista.sort((a, b) => a - b);
+}
+
+function calcularMediana(lista) {
+    let mediana;
+
+    lista = sortList(lista);
+
+    if (lista.length % 2 === 0) {
+        const item = lista[mitadLista2 - 1];
+        const item2 = lista[mitadLista2];
+
+        mediana = calcularMediaAritmetica([item, item2]);
+    } else {
+        mediana = lista[mitadLista2];
+    }
+
+    return mediana;
 }
